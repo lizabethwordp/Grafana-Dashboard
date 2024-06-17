76d45bc463b46ef5917daca4deb0ad86e29c587c
lizabeth <52053436+lizabethwordp@users.noreply.github.com>
2024-06-17 11:44:59 +0100

    Delete data_datasource.tf

--- data_datasource.tf
+++
@@ -1,9 +0,0 @@
-resource "grafana_data_source" "data_mysql_datasource" {
-  name      = var.data_mysql_grafana_name
-  type      = "mysql"
-  url       = var.data_mysql_url
-  database_name  = var.data_mysql_database
-  username = var.data_mysql_username
-  password = var.data_mysql_password
-  
-}
